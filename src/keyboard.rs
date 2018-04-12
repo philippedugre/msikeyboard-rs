ce494f4140e4b0f6ecb3ce4eadbf5badd16a3448
Philippe Dugre <dugre.philippe@hotmail.com>
2018-04-12 13:57:20 -0400

    Now sending Result instead of unwrapping

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,19 +1,18 @@
 use color::Color;
-use hidapi::{HidApi};
+use hidapi::{HidApi, HidError};
 
 struct Keyboard {
     api: HidApi,
 }
 
 impl Keyboard {
-    pub fn new() -> Keyboard {
-        // TODO: Use the ? operator to send a result instead of unwraping.
+    pub fn new() -> Result<Keyboard, HidError> {
         // TODO: Change the HidApi in the struct to a HidDevice
-        let api = HidApi::new().unwrap();
-        Keyboard { api }
+        let api = HidApi::new()?;
+        Ok(Keyboard { api })
     }
 
-    fn write_color(&self, function: u8, zone: u8, color: &Color) {
+    fn write_color(&self, function: u8, zone: u8, color: &Color) -> Result<(), HidError> {
         let data = [
             0x01,
             0x02,
@@ -25,21 +24,15 @@ impl Keyboard {
             0x00,
         ];
 
-        // TODO: Use the ? operator to send a result instead of unwraping
-        self.api
-            .open(0x1770, 0xFF00)
-            .unwrap()
-            .send_feature_report(&data);
+        self.api.open(0x1770, 0xFF00)?.send_feature_report(&data)?;
+        Ok(())
     }
 
-    fn write_mode(&self, mode: u8) {
+    fn write_mode(&self, mode: u8) -> Result<(), HidError> {
         let data = [0x01, 0x02, 0x41, mode, 0x00, 0x00, 0x00, 0x00];
 
-        // TODO: Use the ? operator to send a result instead of unwraping
-        self.api
-            .open(0x1770, 0xFF00)
-            .unwrap()
-            .send_feature_report(&data);
+        self.api.open(0x1770, 0xFF00)?.send_feature_report(&data)?;
+        Ok(())
     }
 }
 
@@ -54,10 +47,10 @@ mod tests {
         let c1 = Color::new(255, 0, 0);
         let c2 = Color::new(0, 255, 0);
         let c3 = Color::new(0, 0, 255);
-        let k = Keyboard::new();
-        k.write_mode(0x01);
-        k.write_color(0x40, 1, &c1);
-        k.write_color(0x40, 2, &c2);
-        k.write_color(0x40, 3, &c3);
+        let k = Keyboard::new().unwrap();
+        k.write_mode(0x01).unwrap();
+        k.write_color(0x40, 1, &c1).unwrap();
+        k.write_color(0x40, 2, &c2).unwrap();
+        k.write_color(0x40, 3, &c3).unwrap();
     }
 }
